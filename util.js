8ab499a0b81344cb23fe941076c37d47c686b348
Steven Hoodikoff <76929918+smhoody@users.noreply.github.com>
2023-07-04 21:35:23 -0400

    removed "add" functionality of saveRoom
    had this in so that the program could tell when the user was adding a room or editing a room but we have updateRoom() so this isnt needed

--- util.js
+++ util.js
@@ -7,7 +7,6 @@ class Database extends React.Component {
     }
 
     static saveRoom = () => {
-        var action = "add";
         var dbObj = localStorage.getItem("rooms");
         var db = JSON.parse(dbObj);
         if (db === null) //if the database hasn't been created yet 
@@ -25,28 +24,25 @@ class Database extends React.Component {
         localStorage.setItem("rooms", JSONObject);
 
         // this will not allow the user to overwrite
-        if (action === "add") { //if room already exists
-            console.log("hi");
-            var button = document.createElement("button");
-            button.type = "button";
-            button.innerHTML = `${roomName} <br> Temp: ${db[roomName].temp}`;
-            button.className = "default-btn btn btn-primary btn-lg m-3 room-btn-custom";
-            button.addEventListener("click", () => {
-                var overlay = document.getElementById("rooms-overlay");
-                var popupBox = document.getElementById("rooms-popupBox");
-                if (this.state.popup === 0) {
-                    overlay.style.display = "block";
-                    popupBox.style.display = "block";
-                    this.state.popup = 1;
-                } else if (this.state.popup === 1) {
-                    overlay.style.display = "none";
-                    popupBox.style.display = "none";
-                    this.state.popup = 0;
-                }
-            });
-            var container = document.getElementById("roomButtons");
-            container.appendChild(button);
-        }
+        var button = document.createElement("button");
+        button.type = "button";
+        button.innerHTML = `${roomName} <br> Temp: ${db[roomName].temp}`;
+        button.className = "default-btn btn btn-primary btn-lg m-3 room-btn-custom";
+        button.addEventListener("click", () => {
+            var overlay = document.getElementById("rooms-overlay");
+            var popupBox = document.getElementById("rooms-popupBox");
+            if (this.state.popup === 0) {
+                overlay.style.display = "block";
+                popupBox.style.display = "block";
+                this.state.popup = 1;
+            } else if (this.state.popup === 1) {
+                overlay.style.display = "none";
+                popupBox.style.display = "none";
+                this.state.popup = 0;
+            }
+        });
+        var container = document.getElementById("roomButtons");
+        container.appendChild(button);
         
     }
     static updateRoom = () => {
